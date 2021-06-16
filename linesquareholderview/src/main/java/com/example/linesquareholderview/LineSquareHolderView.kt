77a05faa9ca0be440444301710f8d1e9a1a199a4
Anwesh43 <anweshthecool0@gmail.com>
2021-06-17 01:54:34 +0530

    adding constants and libraries

--- linesquareholderview/src/main/java/com/example/linesquareholderview/LineSquareHolderView.kt
+++ linesquareholderview/src/main/java/com/example/linesquareholderview/LineSquareHolderView.kt
@@ -1,2 +1,27 @@
 package com.example.linesquareholderview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#01579B",
+    "#00C853",
+    "#FFC107",
+    "#2962FF"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val sizeFactor : Float = 3.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
