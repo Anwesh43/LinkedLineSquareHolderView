418e69de5e95772870e1111a6cb80cf35a513fe0
Anwesh43 <anweshthecool0@gmail.com>
2021-06-17 02:09:32 +0530

    adding Canvas extension methods

--- linesquareholderview/src/main/java/com/example/linesquareholderview/LineSquareHolderView.kt
+++ linesquareholderview/src/main/java/com/example/linesquareholderview/LineSquareHolderView.kt
@@ -29,3 +29,31 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse()).toFloat()
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSquareHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(deg * (1f - 2 * j) * sc2)
+        drawLine(0f, -size * sc5, 0f, -size * sc1, paint)
+        drawRect(RectF(size * sc4, -size, size * sc3, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLSHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSquareHolder(scale, w, h, paint)
+}
